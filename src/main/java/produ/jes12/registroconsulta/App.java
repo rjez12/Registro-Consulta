8e760cd5e27a714548d14d396c422e6848958b7a
atparodi10 <joatorres@uamv.edu.ni>
2026-09-22 18:38:29 -0600

    chore: se elimina archivo HelloController

--- src/main/java/produ/jes12/registroconsulta/App.java
+++ src/main/java/produ/jes12/registroconsulta/App.java
@@ -2,8 +2,8 @@ package produ.jes12.registroconsulta;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class App {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(AppApplication.class, args);
     }
 }
